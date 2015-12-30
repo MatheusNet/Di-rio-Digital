51d25f8ad7f92302c2d710e5d71298c76defba08
MatheusNet <matheus.net10@outlook.com>
2015-12-30 17:38:55 -0200

    Estrutura completada

--- DiarioDigital.TesteUnitario/UnitTest1.cs
+++ DiarioDigital.TesteUnitario/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DiarioDigital.TesteUnitario
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestDD
     {
         [TestMethod]
         public void TestMethod1()
